4a7dd00051b95e303034ad7c3fbcf51b3da960a4
Robert Bost <bostrt@users.noreply.github.com>
2017-06-28 15:25:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from random_words import RandomWords
 
 @route('/')
 def index():
-    return "<h1> RBOST WUZ HERE </h1>"
+    return "<h1> GREEEEEEN (aka B) </h1>"
 
 
 # since this is a read only talk to the replicas
